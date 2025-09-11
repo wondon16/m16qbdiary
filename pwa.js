d34eadaca0142c0eab090e5b65ac45063f68a825
WON <mh16@Mac.lan>
2025-09-11 09:26:30 -0700

    quick update

--- pwa.js
+++
@@ -1,9 +0,0 @@
-// Register service worker for installable app + offline basics
-(() => {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').catch(()=>{});
-    });
-  }
-})();
-
