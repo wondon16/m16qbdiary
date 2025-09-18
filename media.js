97bc56a1833744ad459d7c766fbb95aca21b8bd5
WON <mh16@Mac.lan>
2025-09-17 22:25:58 -0700

    quick update

--- media.js
+++
@@ -1,5 +0,0 @@
-// Optional media helpers placeholder
-// This stub prevents 404s if included but not needed.
-// Add media-related logic here if required later.
-console.debug('media.js loaded');
-
